e4710ffc1058d6e91aaca0e7b035ece31d5c6eca
hwangwy <vogiahuy2105@gmail.com>
2026-09-14 16:50:41 +0700

    orders.csv clean pipeline added

--- tools/cleaner.py
+++ tools/cleaner.py
@@ -26,11 +26,34 @@ def clean_customer(df: pd.DataFrame) -> pd.DataFrame:
     df = df.drop_duplicates(keep='first')
     logger.info("Dropped duplicates")
     df['Age'] = df['Age'].fillna(df['Age'].mean())
-    logger.info("Filled Age NaN values to mean")
+    logger.info("Filled Age Null values to mean")
     df['Age'] = df['Age'].astype(int)
     logger.info("Changed Age dtype to int")
-    df = df.drop_duplicates(subset='City')
-    logger.info("Dropped NaN values from City")
+    df = df.dropna(subset='City')
+    logger.info("Dropped Null values from City")
     df['SignupDate'] = pd.to_datetime(df['SignupDate'])
     logger.info("Formatted SignupDate to type datetime")
+    return df
+
+def clean_orders(df: pd.DataFrame) -> pd.DataFrame:
+    column_format(df)
+    df = df.drop_duplicates(keep='last')
+    logger.info("Dropped duplicates")
+    df = df.dropna(subset='OrderDate')
+    logger.info("Dropped OrderDate Null values")
+    df['OrderDate'] = pd.to_datetime(df['OrderDate'])
+    logger.info("Formatted OrderDate dtype to date")
+    df['Discount'] = df['Discount'].fillna(0)
+    logger.info("Filled Discount Null values with 0")
+    df['Discount'] = df['Discount'] * 100
+    df['Discount'] = df['Discount'].astype(str)
+    df['Discount'] = df['Discount'] + '%'
+    logger.info("Formatted Discount to percentage value")
+    df = df.dropna(subset='Quantity')
+    df['Quantity'] = df['Quantity'].astype(int)
+    logger.info("Changed dtype from float to int for Quantity column")
+    df = df.drop(df[df['Quantity'] < 1].index)
+    logger.info("Dropped outlier values in Quantity column")
+    df['PaymentMethod'] = df['PaymentMethod'].fillna('Cash')
+    logger.info("Filled Null value from PaymentMethod to Cash")
     return df
